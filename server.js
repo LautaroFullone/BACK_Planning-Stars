7e33f96b9cb4bd8f3302d7a94ba9cb605f485993
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-06-11 19:23:40 -0300

    first implementations

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ io.on("connection", socket => {
 
         console.log(`${chalk.green(`${chalk.underline(`Connected device`)}: ${handshake} on ${data.partyID}`)}`); 
 
-        //io.to(data.partyID).emit("playerJoined_socket", ('##' + 'pepe'));
+        //io.to(data.partyID).emit("playerJoined_socket", ('##' + 'pepe')); 
     });
 
     socket.on('disconnect', function () {
@@ -28,52 +28,3 @@ io.on("connection", socket => {
 http.listen(PORT, () => {
     console.log(`\n${chalk.blue(`>> Server listening on port ${PORT}`)}\n`);  
 });
-
-
-
- 
-/*const express = require('express');
-const app = express();
-const chalk = require('chalk');
-const cors = require('cors');
-
-app.use(cors());
-const options = {
-    cors: {
-        origin: 'http://localhost:4200',
-    },
-};
-
-const server = require('http').Server(app);
-const io = require('socket.io')(server, options);
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-server.listen(3000, function () {
-    console.log('\n')
-    console.log(`>> Socket listo y escuchando por el puerto: ${chalk.green('3000')}`)
-})
-
-io.on('connection', function (socket) {
-
-    console.log('connection');
-    const handshake = socket.id;
-
-    let { nameParty } = socket.handshake.query;
-    console.log(`${chalk.green(`Nuevo dispositivo: ${handshake}`)} conentado a la ${nameParty}`);
-    /*socket.join(nameRoom)
-
-    socket.on('evento', (res) => {
-        // Emite el mensaje a todos lo miembros de las sala menos a la persona que envia el mensaje   
-        socket.to(nameRoom).emit('evento', res);
-
-    })
-
-
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
-*/
