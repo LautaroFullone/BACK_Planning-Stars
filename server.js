b416d1701e2e1f27074d90619da5d81a485da0f9
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-06-16 21:51:56 -0300

    last changes

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-io.on("connection", (socket) => {
-    
+io.on("connection", (socket) => {  
     const handshake = socket.id;
 
     console.log(`${chalk.bgGreen(`Connected device: ${handshake}`)}\n`);
@@ -14,9 +13,14 @@ io.on("connection", (socket) => {
     socket.on("joinParty", (data) => {
         let partyID = data.party; let user = data.user;
 
-        socket.join(partyID, () => {
-            socket.emit("actualPlayerJoin_socket", user.name); //event to player that's joining
-            socket.broadcast.to(partyID).emit("playerJoin_socket", user.name);  //event to rest of players (except joinning player)
+        socket.join(partyID, () => {            
+            socket.emit("actualPlayerJoin_socket", user); //event to player that's joining
+            socket.broadcast.to(partyID).emit("playerJoin_socket", user);  //event to rest of players (except joinning player)
+            
+            addDataToSocket(partyID, socket, user);
+
+            sendPartyPlayerEvent(partyID);
+
             console.log(`${chalk.green(`${chalk.underline(`Join party`)}: ${user.name} on ${partyID}`)}\n`); 
         });
     });
@@ -24,18 +28,41 @@ io.on("connection", (socket) => {
     socket.on('leaveParty', (data) => {
         let partyID = data.party; let user = data.user;
 
-        socket.leave(partyID, () => {
-            socket.broadcast.to(partyID).emit("playerLeave_socket", user.name);
-            console.log(`${chalk.red(`${chalk.underline(`Leave party`)}: ${user.name} from party ${partyID}`)}\n`);
-        })
+        deleteSocketFromParty(socket)
+        console.log(`${chalk.red(`${chalk.underline(`Leave party`)}: ${user.name} from party ${partyID}`)}\n`);
     });
  
     socket.on('disconnect', () => {
+        deleteSocketFromParty(socket)
         console.log(`${chalk.bgRed(`Disconnected device: ${handshake}`)}\n`);
     });
     
 });
 
 http.listen(PORT, () => {
-    console.log(`\n${chalk.blue(`>> Server listening on port ${PORT}`)}\n`);  
+    console.log(`\n${chalk.blue(`>> Server listening on port ${PORT}`)}\n`);   
 });
+
+//-------------------------------------------------------------------------------
+
+function sendPartyPlayerEvent(partyID){
+    io.to(partyID).emit("partyPlayers_socket", getPartyPlayers(partyID));
+}
+
+function addDataToSocket(partyID, socket, userData){
+    io.sockets.adapter.rooms[partyID].sockets[socket.id] = userData; 
+    socket.user = partyID;
+    socket.onParty = partyID;
+}
+
+function getPartyPlayers(partyID){
+    if (io.sockets.adapter.rooms[partyID])
+        return io.sockets.adapter.rooms[partyID].sockets
+} 
+
+function deleteSocketFromParty(socket){
+    socket.leave(socket.onParty, () => {
+        sendPartyPlayerEvent(socket.onParty);
+        socket.broadcast.to(socket.onParty).emit("playerLeave_socket", socket.user);
+    })
+}
