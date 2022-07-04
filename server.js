7208f2e54fc8ab94d151379febefea4d2907444d
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-07-04 19:49:42 -0300

    Sprint-10 Completed (#11)
    * PS-143: F_When admin select a US, players should see same item into the screen (#8)
    
    some changes
    
    * PS-144: F_Player list updates (#10)
    
    changes
    
    * PS-143-v2: F_When admin select a US, players should see same item into the screen (#9)
    
    deleting extra class

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+/*  TODO:
+    - the server.js way of work should change
+    - now, when socket is connected, we are going to add a variable name 'planningData'
+    were all neccesary informastion about himseft would be stored. Example:
+    socket {
+        "planningData" = {
+            "user" = {
+                "id": 1,
+                "name": 'Lauti Dev',
+                "email": '***@gmail.com',
+                "password": '*****'
+            }
+            "onParty" = '13ADFA';
+            "moreInfo" = 'additional data'
+        }
+    }
+    - the way of get the list of user connected also is goiung to change
+    - now we can get sokets id's from party with > let sokets = io.sockets.adapter.rooms[partyID].sockets < 
+    and then, for each socket we can get all his data doing this > let data = io.sockets.connected[socketID].planningData; 
+    - the method will function like this: 
+        let list = Object.keys(io.sockets.adapter.rooms[partyID].sockets);
+        list.forEach( element => {
+            console.log(io.sockets.connected[element].planningData )
+        })
+*/
+
 io.on("connection", (socket) => {  
     const handshake = socket.id;
 
@@ -16,13 +42,26 @@ io.on("connection", (socket) => {
         socket.join(partyID, () => {            
             socket.emit("actualPlayerJoin_socket", user); //event to player that's joining
             socket.broadcast.to(partyID).emit("playerJoin_socket", user);  //event to rest of players (except joinning player)
-            
             addDataToSocket(partyID, socket, user);
+            io.to(partyID).emit("partyPlayers_socket", getPartyPlayers(partyID));
 
-            sendPartyPlayerEvent(partyID);
+            if(getSelectedUS(partyID))
+                sendSelectedUS(socket)
 
             console.log(`${chalk.green(`${chalk.underline(`Join party`)}: ${user.name} on ${partyID}`)}\n`); 
         });
+    });
+
+    socket.on('selectUS', (data) => {
+        let userStory = data.us;
+        setSelectedUS(userStory, socket.onParty);
+        sendSelectedUS(socket);
+    });
+
+    socket.on('playerVotation', (data) => { 
+        let votation = data.votation;
+
+        socket.broadcast.to(socket.onParty).emit("playerVotation_socket", votation);
     });
 
     socket.on('leaveParty', (data) => {
@@ -45,24 +84,39 @@ http.listen(PORT, () => {
 
 //-------------------------------------------------------------------------------
 
-function sendPartyPlayerEvent(partyID){
-    io.to(partyID).emit("partyPlayers_socket", getPartyPlayers(partyID));
+function sendSelectedUS(socket) {
+    io.to(socket.onParty).emit("selectedUS_socket", getSelectedUS(socket.onParty))
 }
+
+function setSelectedUS(userStory, partyID){
+    io.sockets.adapter.rooms[partyID].selectedUS = userStory;
+}
+
+function getSelectedUS(partyID) {
+    if (io.sockets.adapter.rooms[partyID])
+        return io.sockets.adapter.rooms[partyID].selectedUS;
+}
+
 
 function addDataToSocket(partyID, socket, userData){
     io.sockets.adapter.rooms[partyID].sockets[socket.id] = userData; 
     socket.user = userData;
     socket.onParty = partyID;
+    /*
+        socket.planningData = {};
+        socket.planningData.user = userData;
+        socket.planningData.onParty = partyID;
+    */
 }
 
 function getPartyPlayers(partyID){
-    if (io.sockets.adapter.rooms[partyID])
+    if(io.sockets.adapter.rooms[partyID])
         return io.sockets.adapter.rooms[partyID].sockets
 } 
 
 function deleteSocketFromParty(socket){
     socket.leave(socket.onParty, () => {
-        sendPartyPlayerEvent(socket.onParty);
+        io.to(socket.onParty).emit("partyPlayers_socket", getPartyPlayers(socket.onParty));
         socket.broadcast.to(socket.onParty).emit("playerLeave_socket", socket.user);
     })
 }
