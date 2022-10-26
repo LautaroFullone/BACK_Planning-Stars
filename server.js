4f69f27e7eda569d6bfc6edc239f13a0884fde3a
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-10-26 16:53:12 -0300

    a

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const http = require('http').Server(app);
 // const io = require('socket.io')(http);
 const io = require("socket.io")(http, {
     cors: {
-        origin: "https://planningstar.vercel.app",
-        methods: ["GET", "POST", "PUT"]
+        origin: "*"
     }
 });
 const chalk = require('chalk');
